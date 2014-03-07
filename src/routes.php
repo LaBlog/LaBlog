a69bc41adf4d343a092f34534085c8847bb996f4
Joseph Ralph <joe@joescode.co.uk>
2014-03-07 21:39:10 +0000

    Updates routes to add /page/ prefix for page numbers

--- src/routes.php
+++ src/routes.php
@@ -9,14 +9,15 @@ $pages = Config::get('lablog::pages');
 Route::pattern('categories', '(.*)');
 Route::pattern('pagename', '('.implode('|', $pages).')');
 Route::pattern('pagenumber', '[0-9]+');
+Route::pattern('page', 'page');
 
 /**
  * Creation of the LaBlog route group with prefix.
  */
 Route::group(['prefix' => $prefix], function() {
     Route::get('/', 'Lablog\Lablog\Controllers\PageController@showPage');
-    Route::get('posts/{pagenumber?}', 'Lablog\Lablog\Controllers\PostController@showPosts');
-    Route::get('category/{categories?}/{pagenumber}', 'Lablog\Lablog\Controllers\CategoryController@showCategory');
+    Route::get('posts/{page?}/{pagenumber?}', 'Lablog\Lablog\Controllers\PostController@showPosts');
+    Route::get('category/{categories?}/{page}/{pagenumber}', 'Lablog\Lablog\Controllers\CategoryController@showCategory');
     Route::get('category/{categories?}', 'Lablog\Lablog\Controllers\CategoryController@showCategory');
     Route::get('{pagename}', 'Lablog\Lablog\Controllers\PageController@showPage');
     Route::get('post/{categories?}/{postname}', 'Lablog\Lablog\Controllers\PostController@showPost');
