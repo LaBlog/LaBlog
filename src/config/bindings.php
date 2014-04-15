344ace49ee0a043aa4a94f4fe807f999a379f756
Joseph Ralph <joe@joescode.co.uk>
2014-04-15 20:52:40 +0100

    Corrected spelling mistake, replaced parsedown with markdown.

--- src/config/bindings.php
+++ src/config/bindings.php
@@ -5,7 +5,7 @@ return array(
      * Register the processor binding.
      * Default: 'Lablog\Lablog\Processor\MarkdownProcessor'
      */
-    'Lablog\Lablog\Processor\ProcessorInterface' => 'Lablog\Parsedown\MarkdownProcessor',
+    'Lablog\Lablog\Processor\ProcessorInterface' => 'Lablog\Markdown\MarkdownProcessor',
 
     /**
      * Register the page type binding.
